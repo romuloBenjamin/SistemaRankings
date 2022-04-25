dfa3f7168e577155ff9c75c04d82c220d8d4bb07
romuloBenjamin <romulo.b.franco@hotmail.com>
2022-04-25 14:52:02 -0300

    update php class - final loopdata

--- src/modules/ranking/ranking-class.php
+++ src/modules/ranking/ranking-class.php
@@ -27,7 +27,7 @@ class Ranking
         return $this->patterns;
     }
     //SQL Rankings Pontos
-    public function sql_find_motorista_by_romaneio()
+    public function sql_romaneio_dados()
     {
         //Data
         $data = "rom_romaneio = '".$this->entry->romaneio."' ORDER BY rom_data_cadastro DESC";
@@ -35,13 +35,31 @@ class Ranking
         $this->sql->pontos = "SELECT rom_data_cadastro, rom_cadastrado_por, rom_motorista FROM intrav2_romaneios WHERE $data";
         return;
     }
-
+    //SQL Ranking Pontos (Por)
+    public function sql_romaneio_cadastro()
+    {
+        //Dados Localização
+        $dados = "fun_id = '".$this->loops["por"]."'";
+        //SQL
+        $this->sql->cadastroPor = "SELECT fun_apelido FROM intrav2_funcionarios WHERE $dados";
+        return $this->sql;
+    }
+    //SQL Ranking Pontos (Motorista)
+    public function sql_romaneio_motorista()
+    {
+        //Dados Localização
+        $dados = "fun_id = '".$this->loops["motorista"]."'";
+        //SQL
+        $this->sql->motorista = "SELECT fun_apelido FROM intrav2_funcionarios WHERE $dados";
+        return $this->sql;
+    }
     //Execute Query
     public function execute_query($sql)
     {
         $execute = new Main("intra");
         $execute->sql = $sql;
-        return $execute->conexao_query();
+        $this->result = $execute->conexao_query();
+        return $this->result;
     }
 
     //Ranking Loopdata
@@ -66,17 +84,45 @@ class Ranking
         return $this->loops;
     }
 
+    //Ranking Loopdata (update)
+    public function ranking_loopdata_update($node)
+    {
+        $this->build["inner"] = array();
+        while($itens = $this->result->fetch_array()) {
+            foreach ($itens as $tags => $content) {
+                if(!is_int($tags)) {
+                    $this->build["inner"][$node] = $content;
+                }
+            }
+            $this->loops[$node] = json_decode(json_encode(array($this->loops[$node],$this->build["inner"][$node])));
+        }
+        $this->build = array();
+        return $this->loops;
+    }
+
     //Initialise Data
     public function init()
     {
         //Get Patterns
         $this->get_patterns_ranking();
         //Set SQL Data
-        $this->sql_find_motorista_by_romaneio();
-        //Execute Query
-        $this->result = $this->execute_query($this->sql->pontos);
+        $this->sql_romaneio_dados();
+        //Execute Query (Dados Romaneio)
+        $this->execute_query($this->sql->pontos);
         //Get Loopdata
         $this->ranking_loopdata();
+        //Get Loopdate (por)
+        $this->sql_romaneio_cadastro();
+        //Execute Query (Update loops)
+        $this->execute_query($this->sql->cadastroPor);
+        //Update Loops (por)
+        $this->ranking_loopdata_update("por");
+        //Get Loopdate (motorista)
+        $this->sql_romaneio_motorista();
+        //Execute Query (Update loops)
+        $this->execute_query($this->sql->motorista);
+        //Update Loops (por)
+        $this->ranking_loopdata_update("motorista");
         //Visualizar Dados
         return $this->visualizar();
     }
